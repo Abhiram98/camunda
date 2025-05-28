9dfeb143b40d1d6543240abdb5c7c4329514e8e5
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-05-28 16:37:13 +0300

    feat: add `ASYNC_REQUEST_METADATA` value type and mappings
    Introduce a new `ASYNC_REQUEST_METADATA` value type for async request metadata records.
    
    Registered the new value type in `ValueTypeMapping` and added support for mapping
    `ValueType` to `Intent` by `Intent#fromProtocolValue` and `Intent#fromProtocolValue` methods.

--- zeebe/protocol/src/main/java/io/camunda/zeebe/protocol/record/intent/Intent.java
+++ zeebe/protocol/src/main/java/io/camunda/zeebe/protocol/record/intent/Intent.java
@@ -74,7 +74,8 @@ public interface Intent {
           BatchOperationIntent.class,
           BatchOperationChunkIntent.class,
           BatchOperationExecutionIntent.class,
-          AdHocSubProcessActivityActivationIntent.class);
+          AdHocSubProcessActivityActivationIntent.class,
+          AsyncRequestMetadataIntent.class);
   short NULL_VAL = 255;
   Intent UNKNOWN = UnknownIntent.UNKNOWN;
 
@@ -194,6 +195,8 @@ public interface Intent {
         return BatchOperationIntent.from(intent);
       case BATCH_OPERATION_PARTITION_LIFECYCLE:
         return BatchOperationIntent.from(intent);
+      case ASYNC_REQUEST_METADATA:
+        return AsyncRequestMetadataIntent.from(intent);
       case NULL_VAL:
       case SBE_UNKNOWN:
         return Intent.UNKNOWN;
@@ -303,6 +306,8 @@ public interface Intent {
         return BatchOperationIntent.valueOf(intent);
       case BATCH_OPERATION_PARTITION_LIFECYCLE:
         return BatchOperationIntent.valueOf(intent);
+      case ASYNC_REQUEST_METADATA:
+        return AsyncRequestMetadataIntent.valueOf(intent);
       case NULL_VAL:
       case SBE_UNKNOWN:
         return Intent.UNKNOWN;
