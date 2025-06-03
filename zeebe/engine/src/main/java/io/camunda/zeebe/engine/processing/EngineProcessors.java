7e024ada32a46112c89f27ff9b3554ae1d2aecc3
Dmitriy Melnychuk <dmitriy.melnychuk@capgemini.com>
2025-06-03 09:46:15 +0300

    feat: emit AsyncRequest events for UT:UPDATE command
    Emit `AsyncRequest:RECEIVED` and `AsyncRequest:PROCESSED` events when handling
    `UserTask:UPDATE` commands.
    
    The processor now emits `AsyncRequest:RECEIVED` during `onCommand`, and in `onFinalizeCommand`,
    it looks up the persisted async request and determines whether the update was triggered by a
    `UT:UPDATE` or `VD:UPDATE` command. The handling logic differs accordingly.
    
    After completing processing, the `AsyncRequest:PROCESSED` event is written.
    This change also removes reliance on `UserTaskTransitionTriggerRequestMetadata`
    in `UserTaskUpdate` processor and applier.

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/EngineProcessors.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/EngineProcessors.java
@@ -197,6 +197,7 @@ public final class EngineProcessors {
             routingInfo,
             clock,
             config,
+            asyncRequestBehavior,
             authCheckBehavior,
             transientProcessMessageSubscriptionState,
             processEngineMetrics);
@@ -216,7 +217,8 @@ public final class EngineProcessors {
         authCheckBehavior);
 
     final var userTaskProcessor =
-        createUserTaskProcessor(processingState, bpmnBehaviors, writers, authCheckBehavior);
+        createUserTaskProcessor(
+            processingState, bpmnBehaviors, writers, asyncRequestBehavior, authCheckBehavior);
     addUserTaskProcessors(typedRecordProcessors, userTaskProcessor);
 
     addIncidentProcessors(
@@ -335,6 +337,7 @@ public final class EngineProcessors {
       final MutableProcessingState processingState,
       final BpmnBehaviorsImpl bpmnBehaviors,
       final Writers writers,
+      final AsyncRequestBehavior asyncRequestBehavior,
       final AuthorizationCheckBehavior authCheckBehavior) {
     return new UserTaskProcessor(
         processingState,
@@ -342,6 +345,7 @@ public final class EngineProcessors {
         processingState.getKeyGenerator(),
         bpmnBehaviors,
         writers,
+        asyncRequestBehavior,
         authCheckBehavior);
   }
 
