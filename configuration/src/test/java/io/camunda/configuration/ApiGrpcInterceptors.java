4309935000e83d70c339d5366a1644ae9a9adf3b
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-09-28 22:20:29 -0600

    seed rename for 144041

--- configuration/src/test/java/io/camunda/configuration/ApiGrpcInterceptors.java
+++ configuration/src/test/java/io/camunda/configuration/ApiGrpcInterceptors.java
@@ -19,9 +19,9 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig({
-  UnifiedConfiguration.class,
-  GatewayBasedPropertiesOverride.class,
-  UnifiedConfigurationHelper.class
+    UnifiedConfiguration.class,
+    GatewayBasedPropertiesOverride.class,
+    UnifiedConfigurationHelper.class
 })
 public class ApiGrpcInterceptors {
 
@@ -37,14 +37,15 @@ public class ApiGrpcInterceptors {
   @Nested
   @TestPropertySource(
       properties = {
-        "camunda.api.grpc.interceptors.0.id=0IdNew",
-        "camunda.api.grpc.interceptors.0.jar-path=0JarPathNew",
-        "camunda.api.grpc.interceptors.0.class-name=0ClassNameNew",
-        "camunda.api.grpc.interceptors.1.id=1IdNew",
-        "camunda.api.grpc.interceptors.1.jar-path=1JarPathNew",
-        "camunda.api.grpc.interceptors.1.class-name=1ClassNameNew"
+          "camunda.api.grpc.interceptors.0.id=0IdNew",
+          "camunda.api.grpc.interceptors.0.jar-path=0JarPathNew",
+          "camunda.api.grpc.interceptors.0.class-name=0ClassNameNew",
+          "camunda.api.grpc.interceptors.1.id=1IdNew",
+          "camunda.api.grpc.interceptors.1.jar-path=1JarPathNew",
+          "camunda.api.grpc.interceptors.1.class-name=1ClassNameNew"
       })
   class WithOnlyUnifiedConfigSet {
+
     final GatewayBasedProperties gatewayCfg;
 
     WithOnlyUnifiedConfigSet(@Autowired final GatewayBasedProperties gatewayCfg) {
@@ -68,18 +69,19 @@ public class ApiGrpcInterceptors {
   @Nested
   @TestPropertySource(
       properties = {
-        "zeebe.gateway.interceptors.0.id=0IdLegacy",
-        "zeebe.gateway.interceptors.0.jarPath=0JarPathLegacy",
-        "zeebe.gateway.interceptors.0.className=0ClassNameLegacy",
-        "zeebe.gateway.interceptors.1.id=1IdLegacy",
-        "zeebe.gateway.interceptors.1.jarPath=1JarPathLegacy",
-        "zeebe.gateway.interceptors.1.className=1ClassNameLegacy"
+          "zeebe.gateway.interceptors.0.id=0IdLegacy",
+          "zeebe.gateway.interceptors.0.jarPath=0JarPathLegacy",
+          "zeebe.gateway.interceptors.0.className=0ClassNameLegacy",
+          "zeebe.gateway.interceptors.1.id=1IdLegacy",
+          "zeebe.gateway.interceptors.1.jarPath=1JarPathLegacy",
+          "zeebe.gateway.interceptors.1.className=1ClassNameLegacy"
       })
   class WithOnlyLegacySet {
-    final GatewayBasedProperties gatewayCfg;
+
+    final GatewayBasedProperties brokerCfg;
 
     WithOnlyLegacySet(@Autowired final GatewayBasedProperties gatewayCfg) {
-      this.gatewayCfg = gatewayCfg;
+      brokerCfg = gatewayCfg;
     }
 
     @Test
@@ -89,7 +91,7 @@ public class ApiGrpcInterceptors {
       final var expectedInterceptorCfg1 =
           createInterceptorCfg("1IdLegacy", "1JarPathLegacy", "1ClassNameLegacy");
 
-      assertThat(gatewayCfg.getInterceptors())
+      assertThat(brokerCfg.getInterceptors())
           .hasSize(2)
           .usingRecursiveFieldByFieldElementComparator()
           .containsExactly(expectedInterceptorCfg0, expectedInterceptorCfg1);
@@ -99,22 +101,23 @@ public class ApiGrpcInterceptors {
   @Nested
   @TestPropertySource(
       properties = {
-        // new
-        "camunda.api.grpc.interceptors.0.id=0IdNew",
-        "camunda.api.grpc.interceptors.0.jar-path=0JarPathNew",
-        "camunda.api.grpc.interceptors.0.class-name=0ClassNameNew",
-        "camunda.api.grpc.interceptors.1.id=1IdNew",
-        "camunda.api.grpc.interceptors.1.jar-path=1JarPathNew",
-        "camunda.api.grpc.interceptors.1.class-name=1ClassNameNew",
-        // legacy
-        "zeebe.gateway.interceptors.0.id=0IdLegacy",
-        "zeebe.gateway.interceptors.0.jarPath=0JarPathLegacy",
-        "zeebe.gateway.interceptors.0.className=0ClassNameLegacy",
-        "zeebe.gateway.interceptors.1.id=1IdLegacy",
-        "zeebe.gateway.interceptors.1.jarPath=1JarPathLegacy",
-        "zeebe.gateway.interceptors.1.className=1ClassNameLegacy"
+          // new
+          "camunda.api.grpc.interceptors.0.id=0IdNew",
+          "camunda.api.grpc.interceptors.0.jar-path=0JarPathNew",
+          "camunda.api.grpc.interceptors.0.class-name=0ClassNameNew",
+          "camunda.api.grpc.interceptors.1.id=1IdNew",
+          "camunda.api.grpc.interceptors.1.jar-path=1JarPathNew",
+          "camunda.api.grpc.interceptors.1.class-name=1ClassNameNew",
+          // legacy
+          "zeebe.gateway.interceptors.0.id=0IdLegacy",
+          "zeebe.gateway.interceptors.0.jarPath=0JarPathLegacy",
+          "zeebe.gateway.interceptors.0.className=0ClassNameLegacy",
+          "zeebe.gateway.interceptors.1.id=1IdLegacy",
+          "zeebe.gateway.interceptors.1.jarPath=1JarPathLegacy",
+          "zeebe.gateway.interceptors.1.className=1ClassNameLegacy"
       })
   class WithNewAndLegacySet {
+
     final GatewayBasedProperties gatewayCfg;
 
     WithNewAndLegacySet(@Autowired final GatewayBasedProperties gatewayCfg) {
