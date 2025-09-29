40afc1a127bc9953f3f1a852b721128286e9d524
Raihan Ullah <41970997+CosmicBeing09@users.noreply.github.com>
2025-09-28 20:08:36 -0600

    seed rename for 103871

--- search/search-domain/src/main/java/io/camunda/search/filter/BatchOperationFilter.java
+++ search/search-domain/src/main/java/io/camunda/search/filter/BatchOperationFilter.java
@@ -21,7 +21,7 @@ public record BatchOperationFilter(
 
   public static final class Builder implements ObjectBuilder<BatchOperationFilter> {
 
-    private List<String> batchOperationIds;
+    private List<String> batchOperationIdOperations;
     private List<String> operationTypes;
     private List<String> state;
 
@@ -30,7 +30,7 @@ public record BatchOperationFilter(
     }
 
     public Builder batchOperationIds(final List<String> values) {
-      batchOperationIds = addValuesToList(batchOperationIds, values);
+      batchOperationIdOperations = addValuesToList(batchOperationIdOperations, values);
       return this;
     }
 
@@ -55,7 +55,7 @@ public record BatchOperationFilter(
     @Override
     public BatchOperationFilter build() {
       return new BatchOperationFilter(
-          Objects.requireNonNullElse(batchOperationIds, Collections.emptyList()),
+          Objects.requireNonNullElse(batchOperationIdOperations, Collections.emptyList()),
           Objects.requireNonNullElse(operationTypes, Collections.emptyList()),
           Objects.requireNonNullElse(state, Collections.emptyList()));
     }
