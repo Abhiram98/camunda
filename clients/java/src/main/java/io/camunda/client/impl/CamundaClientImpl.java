4f94cb6c86b46afe36c569a239d6037b6885c55d
Raihan Ullah <raihan-iict@sust.edu>
2025-10-03 11:12:49 -0600

    commit changes for analysis

--- clients/java/src/main/java/io/camunda/client/impl/CamundaClientImpl.java
+++ clients/java/src/main/java/io/camunda/client/impl/CamundaClientImpl.java
@@ -140,8 +140,8 @@ import io.camunda.client.api.statistics.request.ProcessInstanceElementStatistics
 import io.camunda.client.api.worker.JobClient;
 import io.camunda.client.api.worker.JobWorkerBuilderStep1;
 import io.camunda.client.impl.command.ActivateAdHocSubProcessActivitiesCommandImpl;
-import io.camunda.client.impl.command.AddMappingToGroupCommandImpl;
 import io.camunda.client.impl.command.AssignGroupToTenantCommandImpl;
+import io.camunda.client.impl.command.AssignMappingToGroupCommandImpl;
 import io.camunda.client.impl.command.AssignMappingToTenantCommandImpl;
 import io.camunda.client.impl.command.AssignRoleToClientCommandImpl;
 import io.camunda.client.impl.command.AssignRoleToGroupCommandImpl;
@@ -1142,7 +1142,7 @@ public final class CamundaClientImpl implements CamundaClient {
 
   @Override
   public AssignMappingToGroupStep1 newAddMappingToGroupCommand() {
-    return new AddMappingToGroupCommandImpl(httpClient);
+    return new AssignMappingToGroupCommandImpl(httpClient);
   }
 
   @Override
