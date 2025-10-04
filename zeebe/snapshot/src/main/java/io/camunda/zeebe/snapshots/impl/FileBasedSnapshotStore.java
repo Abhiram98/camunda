2b17022e91a4c5fc3889395239921ceb60f8d34d
Raihan Ullah <raihan-iict@sust.edu>
2025-10-04 13:17:53 -0600

    commit changes for analysis

--- zeebe/snapshot/src/main/java/io/camunda/zeebe/snapshots/impl/FileBasedSnapshotStore.java
+++ zeebe/snapshot/src/main/java/io/camunda/zeebe/snapshots/impl/FileBasedSnapshotStore.java
@@ -96,7 +96,7 @@ public final class FileBasedSnapshotStore extends Actor
   @Override
   public ActorFuture<Void> abortPendingSnapshots() {
     final CompletableActorFuture<Void> abortFuture = new CompletableActorFuture<>();
-    return snapshotStore.purgePendingSnapshots();
+    return snapshotStore.abortPendingSnapshots();
   }
 
   @Override
