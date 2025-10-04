bc4afd419a3b8ce8b7ffb61e29247dfb2d1bd4f6
Raihan Ullah <raihan-iict@sust.edu>
2025-10-04 06:43:20 -0600

    changes to solve benchmark id 128911
     Perform renames that follow this general pattern (case-insensitive): 'key' -> 'id'

--- clients/java/src/test/java/io/camunda/client/tenant/AssignGroupToTenantTest.java
+++ clients/java/src/test/java/io/camunda/client/tenant/AssignGroupToTenantTest.java
@@ -33,7 +33,7 @@ public class AssignGroupToTenantTest extends ClientRestTest {
   @Test
   void shouldAssignGroupToTenant() {
     // when
-    client.newAssignGroupToTenantCommand(TENANT_ID).groupKey(GROUP_KEY).send().join();
+    client.newAssignGroupToTenantCommand(TENANT_ID).groupId(GROUP_KEY).send().join();
 
     // then
     final String requestPath = RestGatewayService.getLastRequest().getUrl();
@@ -50,7 +50,7 @@ public class AssignGroupToTenantTest extends ClientRestTest {
 
     // when / then
     assertThatThrownBy(
-            () -> client.newAssignGroupToTenantCommand(TENANT_ID).groupKey(GROUP_KEY).send().join())
+            () -> client.newAssignGroupToTenantCommand(TENANT_ID).groupId(GROUP_KEY).send().join())
         .isInstanceOf(ProblemException.class)
         .hasMessageContaining("Failed with code 404: 'Not Found'");
   }
@@ -64,7 +64,7 @@ public class AssignGroupToTenantTest extends ClientRestTest {
 
     // when / then
     assertThatThrownBy(
-            () -> client.newAssignGroupToTenantCommand(TENANT_ID).groupKey(GROUP_KEY).send().join())
+            () -> client.newAssignGroupToTenantCommand(TENANT_ID).groupId(GROUP_KEY).send().join())
         .isInstanceOf(ProblemException.class)
         .hasMessageContaining("Failed with code 404: 'Not Found'");
   }
@@ -78,7 +78,7 @@ public class AssignGroupToTenantTest extends ClientRestTest {
 
     // when / then
     assertThatThrownBy(
-            () -> client.newAssignGroupToTenantCommand(TENANT_ID).groupKey(GROUP_KEY).send().join())
+            () -> client.newAssignGroupToTenantCommand(TENANT_ID).groupId(GROUP_KEY).send().join())
         .isInstanceOf(ProblemException.class)
         .hasMessageContaining("Failed with code 500: 'Internal Server Error'");
   }
@@ -92,7 +92,7 @@ public class AssignGroupToTenantTest extends ClientRestTest {
 
     // when / then
     assertThatThrownBy(
-            () -> client.newAssignGroupToTenantCommand(TENANT_ID).groupKey(GROUP_KEY).send().join())
+            () -> client.newAssignGroupToTenantCommand(TENANT_ID).groupId(GROUP_KEY).send().join())
         .isInstanceOf(ProblemException.class)
         .hasMessageContaining("Failed with code 403: 'Forbidden'");
   }
