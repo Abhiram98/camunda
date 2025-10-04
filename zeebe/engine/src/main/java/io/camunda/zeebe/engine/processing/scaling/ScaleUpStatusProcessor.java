7c46c9b9e517323223c5f56321a9524d943d7983
Raihan Ullah <raihan-iict@sust.edu>
2025-10-04 12:10:21 -0600

    commit changes for analysis

--- zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/scaling/ScaleUpStatusProcessor.java
+++ zeebe/engine/src/main/java/io/camunda/zeebe/engine/processing/scaling/ScaleUpStatusProcessor.java
@@ -41,7 +41,7 @@ public class ScaleUpStatusProcessor implements TypedRecordProcessor<ScaleRecord>
     final var desiredPartitions = routingState.desiredPartitions();
     final var response = new ScaleRecord();
     final var desiredPartitionCount = routingState.desiredPartitions().size();
-    final var bootstrappedAt = routingState.bootstrappedAt(desiredPartitionCount);
+    final var bootstrappedAt = routingState.scalingStartedAt(desiredPartitionCount);
     if (bootstrappedAt <= 0) {
       final var reason =
           "Scaling has not started for the desired partition count " + desiredPartitionCount;
@@ -52,7 +52,7 @@ public class ScaleUpStatusProcessor implements TypedRecordProcessor<ScaleRecord>
         desiredPartitions.size(),
         routingState.currentPartitions(),
         routingState.messageCorrelation().partitionCount(),
-        routingState.bootstrappedAt(desiredPartitionCount));
+        routingState.scalingStartedAt(desiredPartitionCount));
     writers.state().appendFollowUpEvent(key, ScaleIntent.STATUS_RESPONSE, response);
     writers.response().writeEventOnCommand(key, ScaleIntent.STATUS_RESPONSE, response, command);
   }
