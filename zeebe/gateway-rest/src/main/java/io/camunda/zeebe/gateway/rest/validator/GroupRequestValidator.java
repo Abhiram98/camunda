36197223c9d9bbc0d380c90b176b17e652308580
Raihan Ullah <raihan-iict@sust.edu>
2025-10-08 11:35:11 -0600

    changes to solve benchmark id 143761
     changes to solve benchmark id 143761 
    
     Perform renames that follow this general pattern (case-insensitive): 'entity' -> ''
    commit changes for analysis
    commit changes for analysis

--- zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/validator/GroupRequestValidator.java
+++ zeebe/gateway-rest/src/main/java/io/camunda/zeebe/gateway/rest/validator/GroupRequestValidator.java
@@ -48,19 +48,19 @@ public final class GroupRequestValidator {
   }
 
   public static void validateMemberId(
-      final String entityId, final EntityType entityType, final List<String> violations) {
+      final String id, final EntityType entityType, final List<String> violations) {
     switch (entityType) {
       case USER:
-        validateId(entityId, "username", violations);
+        validateId(id, "username", violations);
         break;
       case GROUP:
-        validateId(entityId, "groupId", violations);
+        validateId(id, "groupId", violations);
         break;
       case MAPPING_RULE:
-        validateId(entityId, "mappingRuleId", violations);
+        validateId(id, "mappingRuleId", violations);
         break;
       default:
-        validateId(entityId, "entityId", violations);
+        validateId(id, "entityId", violations);
     }
   }
 
